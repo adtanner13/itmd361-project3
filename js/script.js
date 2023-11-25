efcd272ab74741f3114522649a61974cb124cdc3
Drew <143019598+adtanner13@users.noreply.github.com>
2023-11-25 17:12:41 -0600

    google maps test 4

--- js/script.js
+++ js/script.js
@@ -1,27 +1,36 @@
-// Initialize and add the map
-let map;
-
-async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 41.875887589005174, lng: -87.61892990185329 },
+    zoom: 12,
   });
 
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
+  var marker = new google.maps.Marker({
+    position: { lat: 41.88315734286764, lng: -87.62849807716722 },
     map: map,
-    position: position,
-    title: "Uluru",
   });
+
+  var infowindow = new google.maps.InfoWindow({
+    content: "Budlong Hot Chicken",
+  });
+  infowindow.open(map, marker);
+
+  const icons = {
+    thebudlong: {
+      icon: "media/thebudlong.png",
+    },
+  };
+
+  var features = [
+    { position: { lat: 41.88315734286764, lng: -87.62849807716722 }, type: "thebudlong" },
+  ];
+
+  for (let i = 0; i < features.length; i++) {
+    const marker = new google.maps.Marker({
+      position: features[i].position,
+      icon: icons[features[i].type].icon,
+      map: map,
+    });
+  }
 }
 
-initMap();
+google.maps.event.addDomListener(window, 'load', initMap);
