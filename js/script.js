568794495f89a2318c4c39899cdd6fdefda2faf3
Drew <143019598+adtanner13@users.noreply.github.com>
2023-11-25 18:10:41 -0600

    icon change

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
 var map;
 function initMap() {
+
   map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: 41.875887589005174, lng: -87.61892990185329 },
     zoom: 12,
   });
 
-  var marker = new google.maps.Marker({
-    position: { lat: 41.88315734286764, lng: -87.62849807716722 },
-    map: map,
-  });
+  const icons = {
+    budlong: {
+      icon: "media/thebudlong.png"
+    }
+  }
+
+  const features = [
+    {
+      position: new google.maps.LatLng(41.88315734286764, -87.62849807716722),
+      type: "budlong",
+    }
+  ]
+
+  for (let i = 0; i < features.length; i++) {
+    const marker = new google.maps.Marker({
+      position: features[i].position,
+      icon: icons[features[i].type].icon,
+      map: map,
+    });
+  }
 }
 
 initMap();
